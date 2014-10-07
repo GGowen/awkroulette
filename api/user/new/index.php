4976752caac2e03156d7118a9a027b15fa524f1b
Alex Reynolds <cam00674@myport.ac.uk>
2014-10-07 15:31:02 +0100

    Added user exists on signup

--- api/user/new/index.php
+++ api/user/new/index.php
@@ -6,12 +6,13 @@
 // $phonenumber
 include($_SERVER['DOCUMENT_ROOT'] . 'preload.php');
 
-$db->insert([
-	'table' => 'user',
-	'fields' => 'username,password,phonenumber',
-	'values' => "'{$_REQUEST['username']}' , '{$_REQUEST['password']}' , '{$_REQUEST['phone']}'"
-]);
-
-
+if (!user::exists($_REQUEST['username']))
+{
+	$db->insert([
+		'table' => 'user',
+		'fields' => 'username,password,phonenumber',
+		'values' => "'{$_REQUEST['username']}' , '{$_REQUEST['password']}' , '{$_REQUEST['phone']}'"
+	]);	
+}
 
 ?>
