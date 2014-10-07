78b36fa7d4084eaaff9b288eddc0101494baaf15
Gavin Owen <gavinowen16@gmail.com>
2014-10-07 17:40:21 +0100

    new page layout

--- Excuses/index.php
+++ Excuses/index.php
@@ -7,7 +7,7 @@
 	<div class="container">
 		<h1>Get me Out of here!</h1>
 		<form action="">
-			<input class="tel" type="text" placeholder="Give us your number..">
+			<input class="tel" type="text" name="phone" placeholder="Give us your number..">
 			<input class="submit"  type="submit" text="go!">
 		</form>
 	</div>
