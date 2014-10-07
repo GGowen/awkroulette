c77a9bdda4db1e6053ccbeeafe7a8eb2362978a2
Alex Reynolds <cam00674@myport.ac.uk>
2014-10-07 10:22:52 +0100

    Initial Commit

---
+++ app/config/packages/thujohn/twitter/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// You can find the keys here : https://dev.twitter.com/
+
+return array(
+	'API_URL'             => 'api.twitter.com',
+	'API_VERSION'         => '1.1',
+	'USE_SSL'             => true,
+
+	'CONSUMER_KEY'        => 'oxwh0fC8Bx9B4aFicFZo9Su3R',
+	'CONSUMER_SECRET'     => 'GENHzcmH9ny4rOUgTCLXyXm67QsBSOlmY5DZoT5dnTZJFPfgIx',
+	'ACCESS_TOKEN'        => '2418090486-3EeUFnwE8MMvkzKxCVbEQGZx7INJhqS3lPDydJx',
+	'ACCESS_TOKEN_SECRET' => 'LpNQSuoEMU0DxcVsprTwm5LzPKCzmR6khKkMnyRr7SirJ',
+);
---
+++ app/views/client.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('content')
+	<h1 class="jumbotron">World Cup Matches</h1>
+
+	<section class="row">
+		<section class="fixtures col-md-8">
+
+			<h2>Ongoing Fixtures</h2>
+					
+		</section>
+
+		<section id="ticker" class="col-md-4">
+
+			<h2>Latest Events</h2>
+			
+		</section>
+	</section>
+
+@stop
