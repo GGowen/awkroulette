dc2b502f2faebae04155eaadf718d7827d6ec707
Gavin Owen <gavinowen16@gmail.com>
2014-10-07 12:10:59 +0100

    login form

--- index.php
+++ index.php
@@ -2,4 +2,24 @@
 
 <?php echo(__loadResources());?>
 
-<h1>Awk Roullette</h1>
+
+
+<div class="container">
+	<div class="header">
+		<h1>Awk Roullette</h1>
+	</div>
+
+	<div class="login">
+
+	<form class="form-signin" role="form">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <input type="text" class="form-control" placeholder="username" required autofocus>
+        <input type="telephone" class="form-control" placeholder="Telephone" required>
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+		
+	</div>
+</div>
+
