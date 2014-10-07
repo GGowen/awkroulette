42aac3459efda4fa69d96ec157974797f32329d1
Alex Reynolds <cam00674@myport.ac.uk>
2014-10-07 17:29:18 +0100

    Started excuse generator

---
+++ api/excuse/index.php
@@ -0,0 +1,53 @@
+<?php
+
+include($_SERVER['DOCUMENT_ROOT'] . 'preload.php');
+
+$verbs = [
+	'waiting',
+	'angry',
+	'horny',
+	'stupid',
+	'hungry',
+	'boring'
+];
+
+$places = [
+	'the police station',
+	'the hospital',
+	'the strip club',
+	'the brothel',
+	'the pub',
+	'the other party',
+	'school',
+	'work'	
+];
+
+$people = [
+	'girlfriend',
+	'support worker',
+	'wife',
+	'mother',
+	'sister',
+	'brother',
+	'friend',
+	'vicar',
+	'boss'
+];
+
+$activities = [
+	'play football with',
+	'swim with',	
+	'have sex with',
+	'have dinner with',
+	'seduce',
+	'meet',
+	'kill'
+];
+
+$phrases = [
+	"I have to go to the " . $places[array_rand($places)],
+	'My ' . $people[array_rand($people)] . ' is ' . $verbs[array_rand($verbs)],
+	"I need to go and " . $activities[array_rand($activities)] . " my " . $people[array_rand($people)]
+];
+
+echo $phrases[array_rand($phrases)];
