0092174ec4b6aa64920d1aac08962d95a73404e0
Gavin Owen <gavinowen16@gmail.com>
2014-10-07 17:31:06 +0100

    aa

---
+++ Excuses/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+
+	<div class="container">
+		<h1>Get me Out of here!</h1>
+		<form action="">
+			<input class="tel" type="text" placeholder="Give us your number..">
+			<input class="submit"  type="submit" text="go!">
+		</form>
+	</div>
+</body>
+</html>
--- api/user/new/index.php
+++ api/user/new/index.php
@@ -8,11 +8,17 @@ include($_SERVER['DOCUMENT_ROOT'] . 'preload.php');
 
 if (!user::exists($_REQUEST['username']))
 {
-	$db->insert([
+	if($db->insert([
 		'table' => 'user',
 		'fields' => 'username,password,phonenumber',
 		'values' => "'{$_REQUEST['username']}' , '{$_REQUEST['password']}' , '{$_REQUEST['phone']}'"
-	]);	
+	])){echo "true";}
+		else {
+			echo "false";
+		};	
+} else {
+	echo "user already exists";
 }
+
 
 ?>
